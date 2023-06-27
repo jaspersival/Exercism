41d9e8aa555c1ce9462d0c6784ab2afd4f7a59ab
Jasper Sival <jasper.sival@alliander.com>
2023-06-27 14:06:31 +0200

    Implement sublist_at_start

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Comparison {
     Equal,
@@ -12,7 +14,12 @@ pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Compariso
         false => {
             if _first_list == [] { Comparison::Sublist}
             else if _second_list == [] {Comparison::Superlist}
-            else {Comparison::Unequal}
+            else {
+                if _first_list.iter().all( |item| _second_list.iter().any(|item_second_list| item == item_second_list)){
+                    Comparison::Sublist
+                }
+                else {Comparison::Unequal}
+            }
+        }
         }
     }
-}
