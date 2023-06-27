bd730437a5d6395ba184d3947f7ca7601be09ee5
Jasper Sival <jasper.sival@alliander.com>
2023-06-27 17:05:17 +0200

    Implement sublist using a solution example from Exercism

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -7,40 +7,30 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    match _first_list == _second_list {
-        true => Comparison::Equal,
-        false => {
-            if _first_list == [] { Comparison::Sublist}
-            else if _second_list == [] {Comparison::Superlist}
-            else {
-                match _first_list.len() < _second_list.len() {
-                    true => {if is_sublist(_first_list, _second_list){
-                        Comparison::Sublist}
-                        else {Comparison::Unequal}},
-                    false => {
-                            if is_sublist(_second_list, _first_list) {
-                                Comparison::Superlist
-                            }
-                            else {Comparison::Unequal}
-                        }
-                    }
-                }
-            }
+    match (_first_list.len(), _second_list.len()) {
+        (0, 0) => Comparison::Equal,
+        (0, _) => Comparison::Sublist,
+        (_, 0) => Comparison::Superlist,
+        (n, m) if n < m => {
+            if _second_list.windows(n).any(|v| v == _first_list) {
+                Comparison::Sublist
+            } else {
+                Comparison::Unequal
             }
         }
-
-fn is_sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> bool {
-    let first_list_len = _first_list.len();
-    for (index, _) in _second_list.iter().enumerate() {
-        let upper_bound = index + first_list_len;
-        if upper_bound > _second_list.len() {
-            return false
-        }
-        let sublist_second_list = &_second_list[index..(upper_bound)];
-        if sublist_second_list == _first_list {
-            return true
+        (n, m) if n > m => {
+            if _first_list.windows(m).any(|v| v == _second_list) {
+                Comparison::Superlist
+            } else {
+                Comparison::Unequal
             }
+        }
+        (_, _) => {
+            if _first_list == _second_list {
+                Comparison::Equal
+            } else {
+                Comparison::Unequal
+            }
+        }
     }
-    false
 }
-
