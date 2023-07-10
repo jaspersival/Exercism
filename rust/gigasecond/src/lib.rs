6d65fdb2297c607cc6f4c8d61595fb8dc90698cb
Jasper Sival <jasper.sival@alliander.com>
2023-07-10 07:42:02 +0200

    Implemented gigasecond to pass test_date

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,9 @@
+use std::ops::Add;
 use time::PrimitiveDateTime as DateTime;
+use time::{Duration, Time};
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    unimplemented!("What time is a gigasecond later than {start}");
+    let billion_seconds = Duration::seconds(1e9 as i64);
+    start.add(billion_seconds)
 }
