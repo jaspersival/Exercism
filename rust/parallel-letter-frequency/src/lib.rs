1237545ac1c7c9cdd0682645be1670d6db0bc8e3
Jasper Sival <jasper.sival@alliander.com>
2023-08-12 14:10:53 +0200

    Add parallel processing to letter frequency calculation
    The method 'frequency' in 'parallel-letter-frequency' library has been updated to calculate letter frequencies in parallel. This change was implemented to improve performance when processing larger input strings, by dividing the input into chunks and processing each chunk in a separate thread. A new struct 'ParallelLetterFrequency' has been introduced to store the frequencies in a thread-safe manner. Also, a new method 'worker_thread' has been added to handle processing in each thread.

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,15 +1,62 @@
 use std::collections::HashMap;
+use std::hash::Hash;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::thread::JoinHandle;
+
+type FrequencyLetters = HashMap<char, Mutex<usize>>;
+
+#[derive(Debug)]
+struct ParallelLetterFrequency {
+    frequency_letters: Mutex<HashMap<char, usize>>,
+}
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    let mut frequency_letters = HashMap::new();
+    let input = input
+        .into_iter()
+        .map(|x| String::from(*x))
+        .collect::<Vec<String>>();
+    let parallel_letter_frequency = Arc::new(ParallelLetterFrequency {
+        frequency_letters: Mutex::new(HashMap::new()),
+    });
+    let chunk_size = (input.len() + worker_count - 1) / worker_count;
+    let input_chunked = input
+        .chunks(chunk_size)
+        .map(|x| x.to_vec())
+        .collect::<Vec<_>>();
 
-    for word in input {
+    // "https://stackoverflow.com/questions/50282619/is-it-possible-to-share-a-hashmap-between-threads-without-locking-the-entire-has"
+    let threads: Vec<JoinHandle<()>> = input_chunked
+        .into_iter()
+        .map(|input_chunk| {
+            let frequency_letter_clone = Arc::clone(&parallel_letter_frequency);
+            thread::spawn(move || worker_thread(input_chunk, frequency_letter_clone))
+        })
+        .collect();
+    for t in threads {
+        t.join().expect("Thread panicked");
+    }
+    Arc::try_unwrap(parallel_letter_frequency)
+        .expect("Failed to get map from Arc")
+        .frequency_letters
+        .into_inner()
+        .expect("Failed to get map from Mutex")
+}
+fn worker_thread(
+    input_chunk: Vec<String>,
+    parallel_letter_frequency: Arc<ParallelLetterFrequency>,
+) {
+    for word in input_chunk {
         for char in word.to_lowercase().chars() {
             if char.is_alphabetic() {
-                let count = frequency_letters.entry(char).or_insert(0);
+                let mut frequency_letters_guard = parallel_letter_frequency
+                    .frequency_letters
+                    .lock()
+                    .expect("Failed to lock Mutex");
+
+                let count = frequency_letters_guard.entry(char).or_insert(0);
                 *count += 1;
             }
         }
     }
-    frequency_letters
 }
