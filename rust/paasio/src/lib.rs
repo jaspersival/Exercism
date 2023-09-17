8cfb4887de23a40fdb28ee802353273d9da2ed54
Jasper Sival <jasper.sival@alliander.com>
2023-09-17 14:27:41 +0200

    Implement read using read_to_end

--- rust/paasio/src/lib.rs
+++ rust/paasio/src/lib.rs
@@ -1,10 +1,12 @@
+use std::cmp::min;
 use std::io::{Cursor, Read, Result, Write};
 
 // the PhantomData instances in this file are just to stop compiler complaints
 // about missing generics; feel free to remove them
 
 pub struct ReadStats<R> {
-    wrapped: R,
+    data: R,
+    cursor: usize,
     _reads: usize,
 }
 
@@ -12,12 +14,16 @@ impl<R: Read> ReadStats<R> {
     // _wrapped is ignored because R is not bounded on Debug or Display and therefore
     // can't be passed through format!(). For actual implementation you will likely
     // wish to remove the leading underscore so the variable is not ignored.
-    pub fn new(wrapped: R) -> ReadStats<R> {
-        ReadStats { wrapped, _reads: 0 }
+    pub fn new(data: R) -> ReadStats<R> {
+        ReadStats {
+            data,
+            cursor: 0,
+            _reads: 0,
+        }
     }
 
     pub fn get_ref(&self) -> &R {
-        &self.wrapped
+        &self.data
     }
 
     pub fn bytes_through(&self) -> usize {
@@ -25,16 +31,16 @@ impl<R: Read> ReadStats<R> {
     }
 
     pub fn reads(&self) -> usize {
-        self._reads
+        self.cursor
     }
 }
 
 impl<R: Read> Read for ReadStats<R> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        let mut cursor = Cursor::new(&self.wrapped);
-        let position = cursor.read(buf);
+        let result = self.read_to_end(&mut buf.to_vec());
+        self.cursor += buf.len();
         self._reads += 1;
-        position
+        result
     }
 }
 
