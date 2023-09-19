485d1753bf664b6b5c477c7674b61778da342511
Jasper Sival <jasper.sival@alliander.com>
2023-09-19 17:19:50 +0200

    Implement From trait for String, &str and all the unsigned types (using a fancy macro)

--- rust/luhn-from/src/lib.rs
+++ rust/luhn-from/src/lib.rs
@@ -1,8 +1,22 @@
-pub struct Luhn;
+use num_traits::PrimInt;
+use std::fmt::Debug;
+pub struct Luhn {
+    code: String,
+}
 
 impl Luhn {
     pub fn is_valid(&self) -> bool {
-        todo!("Determine if the current Luhn struct contains a valid credit card number.");
+        self.code
+            .chars()
+            .rev()
+            .filter(|c| !c.is_whitespace())
+            .try_fold((0, 0), |(sum, count), val| {
+                val.to_digit(10)
+                    .map(|num| if count % 2 == 1 { num * 2 } else { num })
+                    .map(|num| if num > 9 { num - 9 } else { num })
+                    .map(|num| (sum + num, count + 1))
+            })
+            .map_or(false, |(sum, count)| sum % 10 == 0 && count > 1)
     }
 }
 
@@ -13,6 +27,27 @@ impl Luhn {
 /// Perhaps there exists a better solution for this problem?
 impl<'a> From<&'a str> for Luhn {
     fn from(input: &'a str) -> Self {
-        todo!("From the given input '{input}' create a new Luhn struct.");
+        // 'input' is a borrowed string (&str), can be used directly
+        Self {
+            code: input.to_string(),
+        }
     }
 }
+
+impl From<String> for Luhn {
+    fn from(input: String) -> Self {
+        Self { code: input }
+    }
+}
+macro_rules! impl_trait_for_unsigned {
+    ($($t:ty),*) => {
+        $(
+            impl From<$t> for Luhn {
+                fn from(input: $t) -> Self {
+                    Self { code: input.to_string()}
+                }
+            }
+        )*
+    }
+}
+impl_trait_for_unsigned!(u8, u16, u32, u64, usize);
