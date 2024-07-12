36c5663e932789a150ec0c4c2afcf730ccd73e26
Jasper Sival <jasper.sival@alliander.com>
2024-07-12 18:03:29 +0200

    First test passed

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,35 +1,32 @@
-use std::mem::size_of;
 
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    let mut mut_array = array.clone();
-    mut_array.sort();
+    if array.is_empty() { return None }
 
+    let mut array_copy = array.to_vec();
+    array_copy.sort();
+    let mut slice = array_copy.as_slice();
 
-    let middle_index = get_middle_index(mut_array);
-
-    if mut_array[middle_index] > key {
-        mut_array = &mut_array[..middle_index];
-    }
-
-
-
-
-}
-
-fn get_middle_index(array: &[i32]) -> Option<usize>{
-    if array.is_empty() {
-        None
-    }
-    else {
-        Some(array.len()/2)
-    }
-}
-
-fn get_key(array: &[i32], key: i32) -> Option<usize> {
-    for (idx, item) in array.iter().enumerate() {
-        if *item == key {
-            return Some(idx)
+    while slice.len() > 1 {
+        let middle_index = get_middle_index(slice);
+        if slice[middle_index] == key {
+            return Some(middle_index)
         }
+
+        if slice[middle_index] > key {
+            slice = &slice[..middle_index];
+        }
+        else {
+            slice = &slice[middle_index..];
+        }
+
     }
-    return None
+    return if array_copy[0] == key { Some(0) } else { None }
+
+
+
 }
+
+fn get_middle_index(array: &[i32]) -> usize{
+    array.len()/2
+}
+
