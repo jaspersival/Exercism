7195389af3109a6b8fb0bfff004b2f9a1abe71fa
Jasper Sival <jasper.sival@alliander.com>
2024-07-12 21:26:45 +0200

    Generics work for i32 tests

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,22 +1,22 @@
 
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
+pub fn find<T: Ord + Clone>(array: &[T], key: T) -> Option<usize> {
     if array.is_empty() { return None }
 
     let mut array_copy = array.to_vec();
     array_copy.sort();
 
-    let idx_array_vector: Vec<Vec<i32>> = array_copy.iter().enumerate().map(|(index, &value)| vec![index as i32, value]).collect();
+    let idx_array_vector: Vec<(usize, T)> = array_copy.iter().enumerate().map(|(index, &ref value)| (index, value.clone())).collect();
     let mut slice = idx_array_vector.as_slice();
-    let resulting_index: i32;
+    let resulting_index: usize;
 
     while slice.len() > 1 {
         let middle_index = get_middle_index(slice);
-        if slice[middle_index][1] == key {
-            resulting_index = slice[middle_index][0];
-            return Some(resulting_index as usize)
+        if slice[middle_index].1 == key {
+            resulting_index = slice[middle_index].0;
+            return Some(resulting_index)
         }
 
-        if slice[middle_index][1] > key {
+        if slice[middle_index].1 > key {
             slice = &slice[..middle_index];
         }
         else {
@@ -24,13 +24,13 @@ pub fn find(array: &[i32], key: i32) -> Option<usize> {
         }
 
     }
-    return if slice[0][1] == key { Some(slice[0][0] as usize) } else { None }
+    return if slice[0].1 == key { Some(slice[0].0) } else { None }
 
 
 
 }
 
-fn get_middle_index(array: &[Vec<i32>]) -> usize{
+fn get_middle_index<T: Ord + Clone>(array: &[(usize, T)]) -> usize{
     array.len()/2
 }
 
