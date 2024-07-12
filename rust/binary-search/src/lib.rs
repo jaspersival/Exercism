e6bf7c655da61d9033e5469cfe6ca400fa8c59b5
Jasper Sival <jasper.sival@alliander.com>
2024-07-12 22:14:16 +0200

    Generics work for every test

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,11 +1,12 @@
 
-pub fn find<T: Ord + Clone>(array: &[T], key: T) -> Option<usize> {
+pub fn find<T: AsRef<[U]>, U: Ord + Clone>(array: T, key: U) -> Option<usize> {
+    let array = array.as_ref();
     if array.is_empty() { return None }
 
     let mut array_copy = array.to_vec();
     array_copy.sort();
 
-    let idx_array_vector: Vec<(usize, T)> = array_copy.iter().enumerate().map(|(index, &ref value)| (index, value.clone())).collect();
+    let idx_array_vector: Vec<(usize, U)> = array_copy.iter().enumerate().map(|(index, &ref value)| (index, value.clone())).collect();
     let mut slice = idx_array_vector.as_slice();
     let resulting_index: usize;
 
@@ -30,7 +31,7 @@ pub fn find<T: Ord + Clone>(array: &[T], key: T) -> Option<usize> {
 
 }
 
-fn get_middle_index<T: Ord + Clone>(array: &[(usize, T)]) -> usize{
+fn get_middle_index<U: Ord + Clone>(array: &[(usize, U)]) -> usize{
     array.len()/2
 }
 
