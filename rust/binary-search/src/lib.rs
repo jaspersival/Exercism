c5d2ebea426f7addb9d86afa62455b01b4ac41d9
Jasper Sival <jasper.sival@alliander.com>
2024-07-12 21:04:11 +0200

    Implement working solution for i32 arrays

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -4,15 +4,19 @@ pub fn find(array: &[i32], key: i32) -> Option<usize> {
 
     let mut array_copy = array.to_vec();
     array_copy.sort();
-    let mut slice = array_copy.as_slice();
+
+    let idx_array_vector: Vec<Vec<i32>> = array_copy.iter().enumerate().map(|(index, &value)| vec![index as i32, value]).collect();
+    let mut slice = idx_array_vector.as_slice();
+    let resulting_index: i32;
 
     while slice.len() > 1 {
         let middle_index = get_middle_index(slice);
-        if slice[middle_index] == key {
-            return Some(middle_index)
+        if slice[middle_index][1] == key {
+            resulting_index = slice[middle_index][0];
+            return Some(resulting_index as usize)
         }
 
-        if slice[middle_index] > key {
+        if slice[middle_index][1] > key {
             slice = &slice[..middle_index];
         }
         else {
@@ -20,13 +24,13 @@ pub fn find(array: &[i32], key: i32) -> Option<usize> {
         }
 
     }
-    return if array_copy[0] == key { Some(0) } else { None }
+    return if slice[0][1] == key { Some(slice[0][0] as usize) } else { None }
 
 
 
 }
 
-fn get_middle_index(array: &[i32]) -> usize{
+fn get_middle_index(array: &[Vec<i32>]) -> usize{
     array.len()/2
 }
 
