0cd683e51b60a9437d7127e0d909e3955f9f7b97
Jasper Sival <jasper.sival@alliander.com>
2024-07-22 08:41:02 +0200

    WIP test empty graph

--- rust/dot-dsl/src/lib.rs
+++ rust/dot-dsl/src/lib.rs
@@ -1,9 +1,30 @@
 pub mod graph {
-    pub struct Graph;
+    pub struct Graph {
+        pub nodes: Vec<graph_items::node::Node>,
+        pub edges: Vec<graph_items::edge::Edge>,
+        pub attrs: Vec<(str)>
+    }
 
     impl Graph {
         pub fn new() -> Self {
             todo!("Construct a new Graph struct.");
         }
     }
+
+    pub mod graph_items {
+        pub mod node {
+            pub struct Node {
+                name: str,
+                attr: Vec<(str)>
+            }
+        }
+
+        pub mod edge {
+            pub struct Edge {
+                name: str,
+                attr: Vec<(str)>
+            }
+        }
+    }
+
 }
