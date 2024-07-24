8c60ed11588cafb561063e5af8dc7774e9be6b3f
Jasper Sival <jasper.sival@alliander.com>
2024-07-24 07:51:41 +0200

    WIP make tests compile

--- rust/dot-dsl/src/lib.rs
+++ rust/dot-dsl/src/lib.rs
@@ -2,27 +2,38 @@ pub mod graph {
     pub struct Graph {
         pub nodes: Vec<graph_items::node::Node>,
         pub edges: Vec<graph_items::edge::Edge>,
-        pub attrs: Vec<(str)>
+        pub attrs: Vec<(String)>
     }
 
     impl Graph {
         pub fn new() -> Self {
-            todo!("Construct a new Graph struct.");
+            return Graph {nodes: vec![], edges: vec![], attrs: vec![]}
         }
     }
 
     pub mod graph_items {
         pub mod node {
             pub struct Node {
-                name: str,
-                attr: Vec<(str)>
+                name: String,
+                attr: Vec<(String)>
+            }
+            impl Node {
+                pub fn new(name: String) -> Self {
+                    return Node {name, attr: vec![]}
+                }
             }
         }
 
         pub mod edge {
             pub struct Edge {
-                name: str,
-                attr: Vec<(str)>
+                from_to: (String, String),
+                attr: Vec<(String)>
+            }
+
+            impl Edge {
+                pub fn new(from: String, to: String) -> Self {
+                    return Edge {from_to: (from, to), attr: vec![]}
+                }
             }
         }
     }
