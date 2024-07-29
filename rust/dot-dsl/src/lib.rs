9585eb7612fce75b68b475afdbd477d0bb79573d
Jasper Sival <jasper.sival@alliander.com>
2024-07-29 08:38:35 +0200

    WIP implement methods so that more tests compile

--- rust/dot-dsl/src/lib.rs
+++ rust/dot-dsl/src/lib.rs
@@ -1,6 +1,6 @@
 pub mod graph {
-    pub struct Graph {
-        pub nodes: Vec<graph_items::node::Node>,
+    pub struct Graph<'a> {
+        pub nodes: Vec<graph_items::node::Node<'a>>,
         pub edges: Vec<graph_items::edge::Edge>,
         pub attrs: Vec<(String)>
     }
@@ -9,30 +9,40 @@ pub mod graph {
         pub fn new() -> Self {
             return Graph {nodes: vec![], edges: vec![], attrs: vec![]}
         }
+        pub fn with_nodes(mut self, nodes: &Vec<graph_items::node::Node>) -> Self {
+            self.nodes.extend(nodes);
+            self
+        }
     }
 
     pub mod graph_items {
         pub mod node {
-            pub struct Node {
-                name: String,
-                attr: Vec<(String)>
+            #[derive(Debug, PartialEq)]
+            pub struct Node<'a> {
+                name: &'a str,
+                attrs: Vec<(&'a str,&'a str)>
             }
-            impl Node {
-                pub fn new(name: String) -> Self {
-                    return Node {name, attr: vec![]}
+            impl<'a> Node<'a> {
+                pub fn new(name: &str) -> Self {
+                    return Node {name, attrs: vec![]}
                 }
+                pub fn with_attrs(mut self,attrs: &[(&'a str, &'a str)]) -> Self {
+                    self.attrs.extend_from_slice(attrs);
+                    self
+                }
+
             }
         }
 
         pub mod edge {
             pub struct Edge {
                 from_to: (String, String),
-                attr: Vec<(String)>
+                attrs: Vec<(String, String)>
             }
 
             impl Edge {
                 pub fn new(from: String, to: String) -> Self {
-                    return Edge {from_to: (from, to), attr: vec![]}
+                    return Edge {from_to: (from, to), attrs: vec![]}
                 }
             }
         }
