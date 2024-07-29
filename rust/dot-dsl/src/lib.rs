1f6d032df87da652e9b27275b436470d739ccf95
Jasper Sival <jasper.sival@alliander.com>
2024-07-29 09:48:21 +0200

    WIP implement lifetimes where required

--- rust/dot-dsl/src/lib.rs
+++ rust/dot-dsl/src/lib.rs
@@ -1,16 +1,25 @@
 pub mod graph {
+    #[derive(PartialEq, Debug)]
     pub struct Graph<'a> {
-        pub nodes: Vec<graph_items::node::Node<'a>>,
-        pub edges: Vec<graph_items::edge::Edge>,
-        pub attrs: Vec<(String)>
+        pub nodes: &'a Vec<graph_items::node::Node<'a>>,
+        pub edges: &'a Vec<graph_items::edge::Edge<'a>>,
+        pub attrs: Vec<(&'a str, &'a str)>
     }
 
-    impl Graph {
+    impl<'a> Graph<'a> {
         pub fn new() -> Self {
-            return Graph {nodes: vec![], edges: vec![], attrs: vec![]}
+            return Graph {nodes: &vec![], edges: &vec![], attrs: vec![]}
         }
-        pub fn with_nodes(mut self, nodes: &Vec<graph_items::node::Node>) -> Self {
+        pub fn with_nodes(mut self, nodes: &Vec<graph_items::node::Node<'a>>) -> Self {
             self.nodes.extend(nodes);
+            self
+        }
+        pub fn with_edges(mut self, edges: &Vec<graph_items::edge::Edge<'a>>) -> Self {
+            self.edges.extend(edges);
+            self
+        }
+        pub fn with_attrs(mut self, attrs: &[(&'a str, &'a str)]) -> Self {
+            self.attrs.extend_from_slice(attrs);
             self
         }
     }
@@ -23,7 +32,7 @@ pub mod graph {
                 attrs: Vec<(&'a str,&'a str)>
             }
             impl<'a> Node<'a> {
-                pub fn new(name: &str) -> Self {
+                pub fn new(name: &'a str) -> Self {
                     return Node {name, attrs: vec![]}
                 }
                 pub fn with_attrs(mut self,attrs: &[(&'a str, &'a str)]) -> Self {
@@ -35,14 +44,19 @@ pub mod graph {
         }
 
         pub mod edge {
-            pub struct Edge {
-                from_to: (String, String),
-                attrs: Vec<(String, String)>
+            #[derive(Debug, PartialEq)]
+            pub struct Edge<'a> {
+                from_to: (&'a str, &'a str),
+                attrs: Vec<(&'a str, &'a str)>
             }
 
-            impl Edge {
-                pub fn new(from: String, to: String) -> Self {
+            impl<'a> Edge<'a> {
+                pub fn new(from: &'a str, to: &'a str) -> Self {
                     return Edge {from_to: (from, to), attrs: vec![]}
+                }
+                pub fn with_attrs(mut self, attrs: &[(&'a str, &'a str)]) -> Self{
+                    self.attrs.extend(attrs);
+                    self
                 }
             }
         }
